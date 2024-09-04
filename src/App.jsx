c6d6b9ba3b1c2d5f3307efde39cdb00cabc7a9dd
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-09-05 02:01:29 +0500

    Pushing Code Api Calls and handles (without dom manupulation #003)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import "./App.css";
+// import Weathercard from "./components/Weathercard";
 function App() {
-  const [count, setCount] = useState(0)
+  const userInput = useRef();
+  const [userSearch, setUserSearch] = useState("");
+  const [weatherDataCollection, setWeatherDataCollection] = useState([]);
+  const getUserInput = (evt) => {
+    evt.preventDefault();
+    const inputVal = userInput.current.value;
+    setUserSearch(inputVal);
+
+    userInput.current.value = "";
+  };
+
+  useEffect(() => {
+    const apiCallhandler = async (search) => {
+      if (search != "") {
+        try {
+          const res = await axios(
+            `https://api.weatherapi.com/v1/current.json?key=f442f652b1cb44c59f2173112240409&q=${search}&aqi=no`
+          );
+
+          const data = res.data;
+          setWeatherDataCollection((prev) => {
+            prev.unshift(data);
+            return [...prev];
+          });
+          console.log(weatherDataCollection);
+        } catch (error) {
+          console.log("error===>", error);
+        }
+      } else {
+        console.log("User Search is empty");
+      }
+    };
+    apiCallhandler(userSearch);
+  }, [userSearch]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <article
+        style={{
+          backgroundImage: "url('../public/app-bg-img.gif')",
+          backgroundSize: "cover",
+          backgroundRepeat: "noRepeat",
+          backgroundColor: "rgba(0,0,0,0.5)",
+        }}
+        className="flex flex-col items-center justify-center p-10 g-3"
+      >
+        <h1
+          className="text-[50px] text-[#e5e5e5] font-bold text-center"
+          style={{
+            textShadow: "5px -1px 0px rgba(191,191,191,0.89)",
+          }}
+        >
+          Weather App
+        </h1>
+        <div className="h-[100vh] w-full mt-2">
+          <form className="form w-full flex items-center justify-center gap-1">
+            <input
+              type="text"
+              placeholder="Search for Weather"
+              className="input-field lg:w-[60%] sm:w-[100%] rounded-md p-2 bg-[rgba(0,0,0,0)]"
+              ref={userInput}
+            />
+
+            <button
+              onClick={getUserInput}
+              className="submit-btn btn btn-active btn-neutral"
+            >
+              Search
+            </button>
+          </form>
+        <div>
+          {weatherDataCollection.map((item , index) => (
+           <div key={index}>
+            Weather Card {index}
+           </div>
+          ))}
+        </div>
+        </div>
+      </article>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
