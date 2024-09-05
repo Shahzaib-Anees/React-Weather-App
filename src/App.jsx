3f36690cbdd1a1ca8f6dcad1fbdd0c48d0c0a454
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-09-05 23:10:25 +0500

    Project Completion

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   const [weatherDataCollection, setWeatherDataCollection] = useState([]);
   const getUserInput = (evt) => {
     evt.preventDefault();
+    document.querySelector("#submit-btn").disabled = true;
+    document.querySelector("#submit-btn").innerText = "Loading...";
     const inputVal = userInput.current.value;
     setUserSearch(inputVal);
 
@@ -30,6 +32,10 @@ function App() {
           console.log(weatherDataCollection);
         } catch (error) {
           console.log("error===>", error);
+        }finally{
+          document.querySelector("#submit-btn").disabled = false;
+    document.querySelector("#submit-btn").innerText = "Search";
+
         }
       } else {
         console.log("User Search is empty");
@@ -45,42 +51,90 @@ function App() {
           backgroundImage: "url('../public/app-bg-img.gif')",
           backgroundSize: "cover",
           backgroundRepeat: "noRepeat",
-          backgroundColor: "rgba(0,0,0,0.5)",
         }}
-        className="flex flex-col items-center justify-center p-10 g-3"
       >
-        <h1
-          className="text-[50px] text-[#e5e5e5] font-bold text-center"
+        <article
+          className="flex flex-col items-center min-h-[100vh] max-h-[100%] w-full py-10 px-5 g-3"
           style={{
-            textShadow: "5px -1px 0px rgba(191,191,191,0.89)",
+            backgroundColor: "rgba(0,0,0,0.1)",
           }}
         >
-          Weather App
-        </h1>
-        <div className="h-[100vh] w-full mt-2">
-          <form className="form w-full flex items-center justify-center gap-1">
-            <input
-              type="text"
-              placeholder="Search for Weather"
-              className="input-field lg:w-[60%] sm:w-[100%] rounded-md p-2 bg-[rgba(0,0,0,0)]"
-              ref={userInput}
-            />
+          <h1
+            className="text-[50px] text-[#e5e5e5] font-bold text-center"
+            style={{
+              textShadow: "5px -1px 0px rgba(191,191,191,0.89)",
+            }}
+          >
+            Weather App
+          </h1>
+          <div className="h-[100%] w-full mt-2 flex flex-col items-center justify-center">
+            <form className="form w-full flex items-center justify-center gap-1">
+              <input
+                type="text"
+                placeholder="Search for Weather"
+                className="input-field lg:w-[60%] sm:w-[100%] rounded-md p-2 bg-[rgba(0,0,0,0)]"
+                ref={userInput}
+              />
 
-            <button
-              onClick={getUserInput}
-              className="submit-btn btn btn-active btn-neutral"
-            >
-              Search
-            </button>
-          </form>
-        <div>
-          {weatherDataCollection.map((item , index) => (
-           <div key={index}>
-            Weather Card {index}
-           </div>
-          ))}
-        </div>
-        </div>
+              <button id="submit-btn"
+                onClick={getUserInput}
+                className="submit-btn btn btn-active btn-neutral"
+              >
+                Search
+              </button>
+            </form>
+            <div className="weather-card-container flex flex-col lg:w-[70%] sm:w-[100%] gap-2 items-center justify-center mt-2 p-3">
+              {weatherDataCollection.map((item, index) => (
+                <div
+                  key={index}
+                  className="weather-card bg-[rgba(0,0,0,0.2)] px-[20px] py-2 rounded-md flex flex-col items-center justify-center text-[#ededed] lg:w-[450px] sm:w-[450px]"
+                >
+                  <div className="card-head-content flex flex-col items-center justify-center py-[10px]">
+                    <img
+                      src={item.current.condition.icon}
+                      className="w-[150px]"
+                      alt="icon"
+                    />
+                    <h1 className="text-[38px] font-bold">
+                      {item.location.name}{" "}
+                    </h1>
+                    <h3 className="text-3xl font-bold m-0">
+                      {item.current.temp_c}
+                      <sup>o</sup>C
+                    </h3>
+                    <p className="text-[#f1f1f1] font-bold">
+                      {item.current.condition.text}
+                    </p>
+                  </div>
+                  <div className="card-body-content flex flex-col items-center justify-center w-[100%]">
+                    <p className="border-style text-[#f1f1f1] text-[18px] text-center font-bold w-[100%] p-1">
+                      <span className="text-[28px] px-5">
+                        <i className="fa-solid fa-temperature-low"></i>
+                      </span>
+                      Feels Like : {item.current.feelslike_c}
+                      <sup>o</sup>C
+                    </p>
+                    
+                    <p className="border-style text-[#f1f1f1] text-[18px] text-center font-bold w-[100%] p-1">
+                      <span className="text-[28px] px-5 text-[#a625259f]">
+                        <i className="fa-solid fa-temperature-low"></i>
+                      </span>
+                      Heat Index : {item.current.heatindex_c}
+                      <sup>o</sup>C
+                    </p>
+                    <p className="border-style text-[#f1f1f1] text-[18px] text-center font-bold w-[100%] p-1">
+                      <span className="text-[28px] px-5 ">
+                      <i className="fa-solid fa-wind"></i>
+                      </span>
+                      Heat Index : {item.current.wind_kph}kph
+                    </p>
+                    
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </article>
       </article>
     </>
   );
